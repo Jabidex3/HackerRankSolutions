4072efe5c196d0675666d5a67e82b0f3e05d0f2f
Jabid <jabidmethun@gmail.com>
2021-01-12 16:52:23 -0500

    added misc folder and furniturefactory problem

---
+++ Miscellaneous/FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -0,0 +1,45 @@
+package hackerrank;
+
+import java.util.HashMap;
+
+public class FurnitureOrder implements FurnitureOrderInterface {
+    /**
+     * TODO: Create a map of Furniture items to order quantities
+     */
+
+    /**
+     * Initialize a new mapping of Furniture types to order quantities.
+     */
+    FurnitureOrder() {
+        // TODO: Complete the constructor
+    }
+
+    public void addToOrder(final Furniture type, final int furnitureCount) {
+        // TODO: Complete the method
+    }
+
+    public HashMap<Furniture, Integer> getOrderedFurniture() {
+        // TODO: Complete the method
+        return null;
+    }
+
+    public float getTotalOrderCost() {
+        // TODO: Complete the method
+        return -1.0f;
+    }
+
+    public int getTypeCount(Furniture type) {
+        // TODO: Complete the method
+        return -1;
+    }
+
+    public float getTypeCost(Furniture type) {
+        // TODO: Complete the method
+        return -1.0f;
+    }
+
+    public int getTotalOrderQuantity() {
+        // TODO: Complete the method
+        return -1;
+    }
+}
