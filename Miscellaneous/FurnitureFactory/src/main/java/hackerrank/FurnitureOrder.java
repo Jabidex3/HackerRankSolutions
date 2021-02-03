d758f929a4cbdfa7d24fe3b3a970d4b5e4e056ef
Jabid <jabidmethun@gmail.com>
2021-02-03 14:04:50 -0500

    implemented getTypeCount and getTypeCost methods

--- Miscellaneous/FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ Miscellaneous/FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -8,13 +8,13 @@ public class FurnitureOrder implements FurnitureOrderInterface {
      * TODO: Create a map of Furniture items to order quantities
      */
 
-	private HashMap<Enum<?>,Integer> hm;
+	private HashMap<Furniture, Integer> hm;
     /**
      * Initialize a new mapping of Furniture types to order quantities.
      */
     FurnitureOrder() {
         // TODO: Complete the constructor
-    	hm = new HashMap<Enum<?>, Integer>();
+    	hm = new HashMap<Furniture, Integer>();
     }
 
     public void addToOrder(final Furniture type, final int furnitureCount) {
@@ -24,13 +24,13 @@ public class FurnitureOrder implements FurnitureOrderInterface {
 
     public HashMap<Furniture, Integer> getOrderedFurniture() {
         // TODO: Complete the method
-        return null;
+        return hm;
     }
 
     public float getTotalOrderCost() {
         // TODO: Complete the method
         float cost = 0;
-        for(Map.Entry<Enum<?>, Integer> entry:hm.entrySet()) {
+        for(Map.Entry<Furniture, Integer> entry:hm.entrySet()) {
         	Furniture.valueOf(entry.getKey().toString());
         	//System.out.println((Furniture.valueOf(entry.getKey().toString())).cost());
         	cost+=entry.getValue()*(Furniture.valueOf(entry.getKey().toString())).cost();
@@ -40,16 +40,25 @@ public class FurnitureOrder implements FurnitureOrderInterface {
 
     public int getTypeCount(Furniture type) {
         // TODO: Complete the method
-        return -1;
+    	if(hm.get(type) != null) {
+    		return hm.get(type);
+    	}
+        return 0;
     }
 
     public float getTypeCost(Furniture type) {
         // TODO: Complete the method
-        return -1.0f;
+    	int amt =0;
+    	if(hm.get(type) != null) {
+    		amt =hm.get(type);
+    	}
+    	//System.out.println(amt +" " + type.cost()+ " "+ amt*type.cost());
+        return amt*type.cost();
     }
 
     public int getTotalOrderQuantity() {
         // TODO: Complete the method
-        return -1;
+    	return -1;
+        
     }
 }
